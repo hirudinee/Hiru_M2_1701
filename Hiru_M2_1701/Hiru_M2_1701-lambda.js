746ffb85bf133e882e6cc7cbd64ff3165b5e054e
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-17 12:08:29 +0530

    Kinesis resource and api trigger added

--- Hiru_M2_1701/Hiru_M2_1701-lambda.js
+++ Hiru_M2_1701/Hiru_M2_1701-lambda.js
@@ -1,37 +1,24 @@
 let AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+let SL = require('slappforge-aws');
+
+const kinesis = new AWS.Kinesis();
+
 exports.handler = function (event, context, callback) {
 
-	s3.listObjects({
-		'Bucket': 'hiru.sample',
-		'MaxKeys': 10,
-		'Prefix': ''
+
+	kinesis.describeStream({
+		StreamName: 'MyKStream'
 	}).promise()
-		.then(data => {
-			console.log(data);
-			callback(null, data);          // successful response
-			/*
-			data = {
-			 Contents: [
-				{
-				   ETag: "\\"70ee1738b6b21e2c8a43f3a5ab0eee71\\"",
-				   Key: "example1.jpg",
-				   LastModified: <Date Representation>,
-				   Owner: {
-					  DisplayName: "myname",
-					  ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
-				   },
-				   Size: 11,
-				   StorageClass: "STANDARD"
-				},
-				{...}
-			*/
+		.then(describeStreamData => {
+			callback(null,describeStreamData );
+			console.log(describeStreamData);
 		})
 		.catch(err => {
-			console.log(err, err.stack); // an error occurred
+			callback(null, err);
+			console.log(err);
 		});
 
 
-
-	callback(null, 'Successfully executed');
+	//callback(null, 'Successfully executed');
+	console.log('End of the execution');
 }
